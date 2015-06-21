493dc1d6fe12648f6888c1e1cf63f102bf196235
ritesh2741 <riteshshrestha_007@hotmail.com>
2015-06-21 18:17:54 +0545

    required routes added

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  resources :admin, only: :index
+  resources :doctors
+  resources :patients
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
