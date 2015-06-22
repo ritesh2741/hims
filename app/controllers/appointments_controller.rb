604875339be68eb6c01b67adc09ed5b68eab83d1
ritesh2741 <riteshshrestha_007@hotmail.com>
2015-06-22 11:59:52 +0545

    appointment creation added

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,6 +1,28 @@
-class AppointmentsController < ApplicationController
+class AppointmentsController<ApplicationController
 	def index
-		@doctor = Doctor.find(params[:doctor_id])
-		@appointments = @doctor.appointments
+		@appointments = Appointment.all
 	end
+	def new
+		@patient = Patient.all 
+		@doctor = Doctor.all
+		@appointment = Appointment.new
+	end
+	def create
+		modified_params= appointment_params.merge(doctor_id: params[:doctor_id], patient_id: params[:patient_id])
+		@appointment=Appointment.new(modified_params)
+		respond_to do |format|
+	      if @appointment.save
+	        format.html { redirect_to admin_index_path, notice: 'Appointment was successfully created.' }
+	        format.json { render :show, status: :created, location: @appointment }
+	      else
+	        format.html { render :new }
+	        format.json { render json: @appointment.errors, status: :unprocessable_entity }
+	      end
+		end
+	end
+
+private
+    def appointment_params
+      params.require(:appointment).permit(:schedule, :room)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   resources :admin, only: :index
-  resources :doctors do 
-    resources :appointments
-  end
+  resources :doctors
+  resources :appointments
   resources :patients
 
   # Example of regular route:
