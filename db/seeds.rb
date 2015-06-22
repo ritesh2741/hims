195cd3523a73030d3321f528ce0a10c0ff4ca808
ritesh2741 <riteshshrestha_007@hotmail.com>
2015-06-22 12:01:00 +0545

    seed file for test added.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-department_names = ['Dental', 'Ortho', 'Mental']
-departments = department_names.each{|d| Department.create(name: d)}
+
+departments = ['Orthology','Psychology','Gynaecology','General']
+departments.each do|dept|
+	Department.create(name: dept)
+end
 
 doctors = [
-	{ name: 'Ritesh', description: 'MD, Physician', department: departments.sample }
-	{ name: 'Ritesh2', description: 'MD, Physician' }
-	{ name: 'Ritesh3', description: 'MD, Physician' }
-	{ name: 'Ritesh4', description: 'MD, Physician' }
-]
-
+	{ name: 'J.Asthana', description: 'PhD,MD, Physician', dob: Date.parse('1/12/1950'), gender: 'Male', phone: 9841123456, department_id: 4},
+	{ name: 'Rabina', description: 'MD, Orthologist', dob: Date.parse('1/12/1961'), gender: 'Female', phone: 9841127886, department_id: 1},
+	{ name: 'Sabina', description: 'MD, Gynaecology', dob: Date.parse('1/12/1970'), gender: 'Female', phone: 9841120006, department_id: 3},
+	{ name: 'B.Careful', description: 'MD, Psychiatrist', dob: Date.parse('1/12/1969'), gender: 'Male', phone: 9841778256, department_id: 2}
+	]
 Doctor.create(doctors)
+
+patients = [
+	{ name: 'Tony Hawk', description: 'Fracture Skull', dob: Date.parse('12/05/1951'), gender: 'Male', phone: 9841123456  },
+	{ name: 'Brett Lee', description: 'Stomach Problem', dob: Date.parse('1/12/1950'), gender: 'Male', phone: 9841098456},
+	{ name: 'John', description: 'Headache', dob: Date.parse('1/2/1960'), gender: 'Male', phone: 9841126812 },
+	{ name: 'Ravindra', description: 'Mental Patient', dob: Date.parse('10/2/1956'), gender: 'Male', phone: 9841790456 }
+]
+Patient.create(patients)
+
