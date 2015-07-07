ca0247b5ce6d35bf04402e12ba50b6f32a3fa2de
Ritesh Shrestha <ritesh2741@github.com>
2015-07-07 11:55:08 +0545

    Appointment status and max appointment logic added

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,23 @@ class Appointment < ActiveRecord::Base
 	belongs_to :doctor
 	belongs_to :patient
 	validates_presence_of :doctor
+
+  def self.overload(doctor_id,schedule)
+    appointment_count = Appointment.where(doctor_id: doctor_id, status:'Approved', schedule: schedule ).count
+    if appointment_count < 4
+      return true
+    else
+      return false
+    end
+  end
+
+  def self.check_role(user,appointment)
+    if user.role == 'admin'
+        appointment.status = 'Pending'
+        appointment.save!
+    elsif user.role == 'superadmin'
+        appointment.status = 'Approved'
+        appointment.save!
+    end
+  end
 end
