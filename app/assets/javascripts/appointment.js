7fa061ae4e8dc289938f4ad98451d13ba2c40e7d
Ritesh Shrestha <ritesh2741@github.com>
2015-07-12 08:52:03 +0545

    Code Refactor

--- app/assets/javascripts/appointment.js
+++ app/assets/javascripts/appointment.js
@@ -1,5 +1,3 @@
-var today = new Date().toISOString().split('T')[0];
 $(document).ready(function(){
-    $('.selectpicker').selectpicker();
-    document.getElementsByName("schedule")[0].setAttribute('min', today);
-  })
+  $('.selectpicker').selectpicker();
+})
